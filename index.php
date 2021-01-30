0f683a52d1cd211255cbd2cd02b332b2270a5bc4
EZeX377 <75936212+EZeX377@users.noreply.github.com>
2021-01-30 10:03:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,370 @@
+ 
+<!DOCTYPE html>
+<html lang="en" style="width: 100%;">
+<head>
+    <title>Zayne - Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@300;400&family=Archivo+Black&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Raleway&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/421f97763e.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   
+    <link href="main7.css" rel=stylesheet>        
+    </head>
+
+<style>
+    h1{font-size: 2.5em;}
+.overlay{position: relative;width: 100%;height: 100%;background-color: rgba(0,0,0,0.8);display: block;}
+    .htxt{display: flex;align-items: center;width: 100%;}
+    .herotxt{padding: 0;}
+    .swrap{font-size: 5.3em;font-weight: bold;animation: sld 15s ease-in-out infinite;}
+    .swrap a {color: #fff;text-decoration: none;}
+    /*.stxt1{padding: 10px 20px;animation: slideIn .5s, pulse 1s .5s;position: relative;transform-origin: left center;}
+    .stxt2, .stxt3, .stxt4{padding: 10px 20px;}
+    .stxt2{animation: slideIn 1.5s, pulse 1s 1.5s;position: relative;transform-origin: left center;}
+    .stxt3{animation: slideIn 3s, pulse 1s 3s;position: relative;transform-origin: left center;}
+    .stxt4{animation: slideIn 4.5s, pulse 1s 4.5s;position: relative;transform-origin: left center;}*/
+    .stxt1,.stxt2,.stxt3,.stxt4,.stxt5{display: inline-block;width: 100%;position: absolute;padding: 10px 20px;}
+    .stxt1{animation: pulse 1s .3s;transform-origin: left center;}
+    .stxt2{margin-left: 100%;animation: pulse 1s 3.7s;transform-origin: left center;}
+    .stxt3{margin-left: 200%;animation: pulse 1s 7.4s;transform-origin: left center;}
+    .stxt4{margin-left: 300%;animation: pulse 1s 11.1s;transform-origin: left center;}
+    .stxt5{margin-left: 400%;}
+    .swrap i{font-style: normal;color: #67BC44;}
+    .nav .lnk a:hover{color: #FE0000;}
+    .hero{overflow: hidden;}
+    .herotxt{width: 100%;}
+    @keyframes sld {
+        0%{margin-left: 0%;}
+        17%{margin-left: 0%;}
+        24%{margin-left: -100%;}
+        42%{margin-left: -100%;}
+        49%{margin-left: -200%;}
+        67%{margin-left: -200%;}
+        74%{margin-left: -300%;}
+        92%{margin-left: -300%;}
+        99%{margin-left: -400%;}
+        100%{margin-left: -400%;}
+    }
+    @-webkit-keyframes pulse{0%{-webkit-transform:scaleX(1);transform:scaleX(1)}25%, 75%{-webkit-transform:scale3d(1.2,1.2,1.2);transform:scale3d(1.2,1.2,1.2)}to{-webkit-transform:scaleX(1);transform:scaleX(1)}}
+    @keyframes pulse{0%,50%{-webkit-transform:scaleX(1);transform:scaleX(1)}25%, 75%{-webkit-transform:scale3d(1.2,1.2,1.2);transform:scale3d(1.2,1.2,1.2)}to{-webkit-transform:scaleX(1);transform:scaleX(1)}}
+    @keyframes slideIn {
+        0%{opacity: 0;left: -700px;}
+        65%{opacity: 0;left: -350px;}
+        100%{opacity: 1;left: 0;}
+    }
+    .teamimg2{width: 100%;text-align: center;display: none;}
+    .tmcon{height: 500px;background-color: #67BC44;}
+    .lgo{height: 90px;}
+    .founimg{width: 300px;}
+    .minnav{padding: 15px;display: none;float: right;border: none;background-color: transparent;outline: none;}
+    .dropcon{display: none;position: absolute;box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);background-color: #F7F7F7;top: 74px;left: 0;padding: 8px 5px;width: 100%;}
+    .dropcon a{display: block;padding: 15px;}
+    .dropcon a:hover{color: #FE0000;}
+    .minnav:hover{color: #FE0000;outline: #FE0000;}
+    .gobot{position: absolute;bottom: 130px;left: 50%;font-size: 1.3em;font-weight: bold;width: 500px;}
+    .gobot p{padding: 0px;margin: 0px;}
+    .gobot .l1{animation: slide 8s ease-in-out;}
+    .gobot .l2{animation: slideb 9s ease-in-out;width: 500px;}
+    .gobot i{font-style: normal;color: #FE0000;font-weight: bold;font-size: 1.5em;}
+    .btt{background-color: #fff;}
+        .scroll-downs {
+  position: absolute;
+  left: 50%;
+  bottom: 140px;  
+  width :34px;
+  height: 55px;
+  transform: translate(-50%,0%);
+}
+.mousey {
+  width: 3px;
+  padding: 10px 15px;
+  height: 35px;
+  border: 2px solid #fff;
+  border-radius: 25px;
+  opacity: 0.75;
+  box-sizing: content-box;
+}
+.scroller {
+  width: 3px;
+  height: 10px;
+  border-radius: 25%;
+  background-color: #fff;
+  animation-name: scroll;
+  animation-duration: 1.5s;
+  animation-timing-function: cubic-bezier(.15,.41,.69,.94);
+  animation-iteration-count: infinite;
+}
+@keyframes scroll {
+  0% { opacity: 0; }
+  10% { transform: translateY(0); opacity: 1; }
+  100% { transform: translateY(15px); opacity: 0;}
+}
+h1{color: #015A2E;}
+.pnts {width: 70%;padding:10px 25px;background-color: #ddd;}
+    .pnts ul{list-style-type: none;padding: 0;margin: 0;}
+    .pnts li{background: url('bullet4.png') no-repeat left top;min-height: 54px;padding-left: 38px;padding-top: 0px;padding-bottom: 5px;font-size: 19.2px;color: #777;}
+    .res{width: 30%;padding: 134px 10px;background-color: #FE0000;color: #fff;}
+    #res{padding-top: 150px;}
+    .prmhdsm{width: 100%;display: none;}
+    .prmhd{width: 30%;}
+    .prmtxt{width: 70%;padding:10px 25px;background-color: #eee;}
+    @media only screen and (max-width: 600px){
+        .contain{padding: 50px 10px;}
+        .lgo{height: 50px;}
+        .swrap{font-size: .7em;height: 30px;}
+        .abt h1{font-size: 1.5em;}
+        .abt p{font-size: 1em;}
+        .abt{width: 100%;padding: 35px 20px;}
+        .abt2{display: none;}
+        .serv{width: 100%;height: auto;padding: 15px;text-align: center}
+        .servcon{width: 100%;height: auto;background-color: #fff;padding: 25px;}
+        .servtxt{width: 100%;height: auto;border: 1px solid #ccc;padding: 15px 10px;}
+        .servtxt p{letter-spacing: 1px;color: #777;}
+        .prom{padding: 35px 20px;}
+        .prom h1{font-size: 1.5em;}
+        .prom p{letter-spacing: 1px;font-size: 1em;color: #777;}
+        .team h1{font-size: 1.5em;}
+        .team p{letter-spacing: 1px;font-size: .9em;color: #fff;}
+        .teamtxt{width: 100%; padding: 20px;}
+        .teamimg{width: 100%;text-align: center;display: none;}
+        .teamimg2{width: 100%;text-align: center;display: block;}
+        .tmcon{height: auto;}
+        .founimg{width: 200px;}
+        .contact .col{width: 100%;}
+        .confrm{padding: 0 20px;}
+        .hero{height:400px;}
+        .herotxt{width: 100%;}
+        .nav .lnk{padding: 10px 0;}
+        .nav .lnk a{font-size: 1em;}
+        .pnts li{padding-left: 38px;padding-top: 0px;padding-bottom: 5px;}
+            .pnts {width: 100%;padding:10px ;}
+            .res{width: 100%}
+            .prmhdsm{display: contents;}
+    .prmhd{display: none;}
+    .prmtxt{width: 100%;}
+   
+    .minnav{display: block;}
+    .lrghd{display: none;} 
+
+    .scroll-downs {
+  position: absolute;
+  left: 50%;
+  bottom: 0px;  
+  width :34px;
+  height: 55px;
+  transform: translate(-60%,-50%);
+  transform: scale(.7);
+}   
+    } 
+     }
+</style>
+<body>
+    <div class="nav" style="box-shadow: 0px 5px 5px rgba(0,0,0,0.6);background-color: #F7F7F7;">    
+        <a href="" style="" class="lnkimg"><img src="logo4.jpeg" style="" alt="logo" class="lgo">
+        <div class="lnk">
+        <a href="" class="lrghd">Home</a>
+        <a href="#about" class="lrghd">About</a>
+        <a href="services.php" class="lrghd">Services</a>
+        <a href="#contact" class="lrghd">Contact</a>
+        <button class="minnav" id="minnav"><i class="fas fa-bars"></i></button>
+        <div class="dropcon" id="dropcon">
+            <a href="">Home</a>
+            <a href="#about">About</a>
+            <a href="services.php">Services</a>
+            <a href="#contact">Contact</a>
+        </div>
+        </div>
+    </div>
+    <div class="btt">
+            <a href="#home"><center><img src="btt.png" style="height: 40px;" alt="bttlogo"></center></a>
+        </div>
+    <div class="hero" id="home">
+        <div class="overlay">
+            <div class="herotxt">
+                <div class="htxt">
+                    <div class="swrap">
+                        <a href="services.php">
+                        <div class="stxt1">Do you have a <i>property</i> in Assam?</div>
+                        <div class="stxt2">Are you staying away from your <i>property</i>?</div>
+                        <div class="stxt3">Having problems renting out your <i>property</i>?</div>
+                        <div class="stxt4">Do you travel frequently to manage your <i>property</i>?</div>
+                        <div class="stxt5">Do you have a <i>property</i> in Assam?</div></a>
+                    </div>
+                </div>
+            </div>
+            <a href="#res"><div class="scroll-downs">
+
+              <div class="mousey">
+                <div class="scroller"></div>
+
+              </div>
+            </div></a>
+            <!--<div class="gobot">
+                <p class="l1">If <i>YES?</i></p><p class="l2">Our services can help you extensively</p>
+            </div>-->
+        </div>
+    </div>
+    <div class="contain" id="res">
+        <div class="row">
+            <div class="col res" style="">
+                <center><h1 style="color: #fff;">7 Reasons to<br>choose us</h1></center>
+            </div>
+            <div class="col pnts" style="">
+                <ul>
+                    <li>We deposit rent in your account every month right on time.</li>
+                    <li>We have the best customer service that is capable of handling all customer queries.</li>
+                    <li>We have an extremely transparent work process so rest assured you would know what you are dealing with in each part of the process.</li>
+                    <li>We do a thorough background check on each tenant to ensure safety of the property.</li>
+                    <li>We give complete control over your property from deciding rent to tenant profile approval.</li>
+                    <li>You pay us only when you start receiving your rents and there are no other hidden charges.</li>
+                    <li>We ensure the quality of your property is maintained by providing half yearly maintenance check and also at the time of onboarding new tenant and exiting old tenants.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="contain row" id="about">
+        <div class=" col abt2">
+            <img src="aboutus.jpg" style="height: 400px"; alt="about">
+        </div>
+        <div class="col abt">
+            <h1>About Us</h1>
+            <p>A newly established company located in Guwahati - the heart of Assam and the gateway to the North East of India. Zayne is started by a highly qualified and experienced professional from the service and payment industry with a goal to simplify and ease property ownership and management thereby helping home owners living away to maximize returns through safe and secure processes.</p>
+        </div>
+    </div>
+        
+        
+    <div style="background-color: #eee;" id="service">
+    <div class="contain">
+        <center><h1>What we offer ?</h1></center>
+        <div class="row">
+            <div class="col serv">
+                <div class="servcon">
+                    <div class="servtxt">
+                        <center><div style="height: 100px;width: 100px;"><img src="propertythin.png" style="height: 100px;" alt="propertylogo"></div></center>
+                        <h3>Property management</h3>
+                        <p>Inventory checks both interior & exterior at the time of tenant move-in and exit, minor and regular repair/maintenance, six monthly inspection visits and upkeep of the property, Providing real time video and photos to owners.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col serv">
+                <div class="servcon">
+                    <div class="servtxt">
+                        <center><div style="height: 100px;width: 100px;"><img src="rent.png" style="height: 100px;" alt="rentlogo"></div></center>
+                        <h3>Tenant management</h3>
+                        <p>Tenant search, facilitate tenant move-in/out, police verification, legal documentations and society formalities, seamless renewal agreement or tenant replacement</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col serv">
+                <div class="servcon">
+                    <div class="servtxt">
+                        <center><div style="height: 100px;width: 100px;"><img src="tenantpng.png" style="height: 100px;" alt="tenantlogo"></div></center>
+                        <h3>Rent &amp; Deposit management</h3>
+                        <p>Collection of security deposit & monthly rent. Rent transfer, bill payment and society payment. Exit settlement</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col serv">
+                <div class="servcon">
+                    <div class="servtxt">
+                        <center><div style="height: 100px;width: 100px;"><img src="control.png" style="height: 100px;" alt="propertycontrollogo"></div></center>
+                        <h3>Your Property - Your Control</h3>
+                        <p>Tenants are finalised post your approval. All expenses, including minor one are approved by you.<br>Our agreements are simple with no hidden charges and detailed account statements will be provided preodically.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="contain">
+        <img src="new8.jpg" style="width: 100%;"  alt="flochart">
+    </div>
+    <div class="contain"  style="" >
+        <div class="prom">
+            <div class="row">
+                <div class="col prmhdsm" style="">
+                    <center><h1>We Promise</h1></center>
+                 </div>
+                 <div class="col prmtxt" style="">
+                    <p>Complete transparency and honesty is what we provide.</p>
+                    <p>To cater to the entire population of home owners living away.</p>
+                 </div>
+                 <div class="col prmhd" style="">
+                    <center><h1>We Promise</h1></center>
+                 </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="tmcon" style="">
+        <div class="contain team row"> 
+            <div class="teamimg2 col">
+                <img src="SIMANTA%20-%20Simanta%20Baishya.jpg" style="" alt="founder" class="founimg">
+            </div>       
+            <div class="teamtxt col">
+                <h1>Our Team</h1>
+                <p><b>Founder</b></p>
+                <hr style="">
+                <p>Simanta Baishya, born and brought up in Tezpur and Guwahati, Assam is a post graduate from Edinburgh Business School and has about 22 years of experience in the Banking, Insurance and Payments Industry with companies like GE, Citibank, Pinelabs. Simanta is an innovative, dynamic and a result oriented professional with great organisational and strategic skills.</p>
+            </div>
+            <div class="teamimg col">
+                <img src="SIMANTA%20-%20Simanta%20Baishya.jpg" style="" alt="founder" class="founimg">
+            </div>
+        </div>
+    </div>
+    <div class="contain" id="contact"  style="background-color: #eee;">
+        <div class="contact row">
+            <center><h1>Get in Touch</h1>
+                <p>For more info or inquiry about out services, pricing please feel free to drop a message</p></center>
+                <br><br>
+            <div class="col confrm">
+                <br><br>
+            <form>
+                <input type="text" name="" placeholder="Name">
+                <input type="email" name="" placeholder="Email">
+                <textarea name="" rows="7" placeholder="Enter your message"></textarea>
+                <button>SEND</button>
+            </form>
+            </div>
+            <div class="col">
+                <div class="contxt">
+                    <p><i class="fas fa-phone-alt"></i> 99888-83143</p>
+                    <p><i class="far fa-map"></i> DS Mansion, Ground floor, RG Baruah road, Ghy - 781005</p>
+                    <p><i class="fas fa-at"></i> email address</p>
+                    <a href="https://www.google.com/maps/place/DS+Mansion/@26.1627592,91.7804625,19.25z/data=!4m12!1m6!3m5!1s0x375a59f96e44bc49:0xb59f2a8e2d66ccbe!2sDS+Mansion!8m2!3d26.1627074!4d91.7810183!3m4!1s0x375a59f96e44bc49:0xb59f2a8e2d66ccbe!8m2!3d26.1627074!4d91.7810183" target="_blank"><img src="maps3.jpg" style="width: 100%;height: 300px;object-fit: cover;" alt="map"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="contain2">
+        <div class="footer">
+            <div class="slink">
+                <a href=""><i class="fab fa-facebook" style=""></i></a>
+                <a href=""><i class="fab fa-instagram" style=""></i></a>
+                <a href=""><i class="fab fa-twitter" style=""></i></a>
+            </div>
+           <p>&copy; 2020 ZAYNE. All rights reserved.</p> 
+        </div>
+    </div>
+<script>
+$(document).ready(function() {
+  $('#minnav').click(function() {
+    $("#dropcon").toggle();
+  });
+});
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 600) {
+    $('.btt').fadeIn();
+  } else {
+    $('.btt').fadeOut();
+  }
+});
+</script>
+
+</body>
+</html>
